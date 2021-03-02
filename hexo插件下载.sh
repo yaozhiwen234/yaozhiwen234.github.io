0f3837feb6ada6665996fe1f0d60b3e4f8d0db42
yaozhiwen234 <2718486004@qq.com>
2021-03-02 11:09:43 +0800

    Site updated: 2021-03-02 11:09:42

---
+++ hexo插件下载.sh
@@ -0,0 +1,14 @@
+npm install hexo-wordcount --save &&
+npm install hexo -renderer -pug hexo -renderer -stylus - -save &&
+npm install --save hexo-tag-aplayer &&
+npm install - -global gulp -cli &&
+npm install gulp -htmlclean - -save -dev &&
+npm install - -save gulp -html -minifier -terser &&
+npm install gulp -clean -css - -save -dev &&
+npm install gulp -terser - -save -dev &&
+npm install - -save -dev gulp -uglify &&
+npm install gulp-babel --save-dev &&
+npm install babel-preset-env --save-dev &&
+npm install babel-core --save-dev &&
+npm install - -save -dev gulp -imagemin &&
+npm install workbox -build gulp - -save -dev
